b24d7f2fd40fc06d86303e3a636b40e0afbdd763
hunf299 <150046446+hunf299@users.noreply.github.com>
2024-03-26 22:01:55 +0700

    Delete B1 DB directory

--- B1 DB/src/GK2022/Implement.java
+++
@@ -1,8 +0,0 @@
-package GK2022;
-
-public interface Implement {
-    double checkAccount(String id, String accountNum);
-    boolean transferMoney(Customer sender, Customer receiver, double amount);
-    double addMoney(double moneyADD);
-    double withdrawMoney(double moneyWD);
-}
